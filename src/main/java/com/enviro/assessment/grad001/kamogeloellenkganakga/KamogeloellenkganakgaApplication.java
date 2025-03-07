ad7ad8a12419721b0ede3252b04ed7f6e5322b45
KamoEllen <kamoellenkganakga@gmail.com>
2025-03-07 20:01:43 +0200

    Adding starter,db config

---
+++ src/main/java/com/enviro/assessment/grad001/kamogeloellenkganakga/KamogeloellenkganakgaApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.kamogeloellenkganakga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamogeloellenkganakgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KamogeloellenkganakgaApplication.class, args);
+	}
+
+}
